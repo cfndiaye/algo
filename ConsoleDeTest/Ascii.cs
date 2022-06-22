358d43def5fe82e1b1af7276afaabf2d37ca2a8c
cfndiaye <cfndiaye@gmail.com>
2022-06-22 15:14:51 +0000

    optimise ascii

---
+++ ConsoleDeTest/Ascii.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDeTest
+{
+    public class Ascii
+    {
+        public static char[,] GetAscii(char c)
+        {
+            var MonDictionnaire = new Dictionary<char, char[,]>();
+            char[,] A = new char[4, 5];
+            A[0, 0] = ' '; A[0, 1] = '#'; A[0, 2] = '#'; A[0, 3] = '#'; A[0, 4] = '#';
+            A[1, 0] = '#'; A[1, 1] = ' '; A[1, 2] = '#'; A[1, 3] = ' '; A[1, 4] = ' ';
+            A[2, 0] = ' '; A[2, 1] = '#'; A[2, 2] = '#'; A[2, 3] = '#'; A[2, 4] = '#';
+            A[3, 0] = ' '; A[3, 1] = ' '; A[3, 2] = ' '; A[3, 3] = ' '; A[3, 4] = ' ';
+
+
+            MonDictionnaire.Add('A', A);
+            if (MonDictionnaire.ContainsKey(c))
+            {
+                return MonDictionnaire[c];
+            }
+            else
+            {
+                return null;
+            }
+
+
+        }
+    }
+}
--- ConsoleDeTest/Program.cs
+++ ConsoleDeTest/Program.cs
@@ -10,33 +10,36 @@ namespace ConsoleDeTest
             Console.WriteLine("Entrer un texte: ");
             var entree = Console.ReadLine();
             char[] dataEntrer = entree.ToCharArray();
-            char[,] montableau = new char[8, 5];
+            var nc = dataEntrer.Length;
+            var dim = nc * 4;
+            char[,] montableau = new char[dim, 5];
+            var listAscii = new List<char[,]>();
+            for (int i = 0; i < nc; i++)
+            {
+                var ascii_ = Ascii.GetAscii(dataEntrer[i]);
+                listAscii.Add(ascii_);
+            }
+            
+            int count = 0;
+            foreach (var item in listAscii)
+            {
+                
+                for (int j = 0; j < 5; j++)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
 
-            char[,] ascii = GetAscii(dataEntrer[0]);
-            char[,] ascii2 = GetAscii(dataEntrer[1]);
-           // ascii.CopyTo(montableau,0);
-           // ascii2.CopyTo(montableau, 4);
+                        montableau[i + count, j] = item[i, j];
+                    }
+                }
+                count += 4;
+            }
            
-                for (int j = 0; j < 5; j++)
-                {
-                    for (int i = 0; i < 4; i++)
-                    {
-                        
-                        montableau[i, j] = ascii[i,j];
-                    }
-                }
-                for (int j = 0; j < 5; j++)
-                {
-                    for (int i = 0; i < 4; i++)
-                    {
-
-                        montableau[i+4, j] = ascii2[i, j];
-                    }
-                }
+              
 
             for (int j = 0; j < 5; j++)
             {
-                for (int i = 0; i < 8; i++)
+                for (int i = 0; i < dim; i++)
                 {
 
                     Console.Write(montableau[i, j]);
@@ -45,41 +48,9 @@ namespace ConsoleDeTest
             }
 
 
-            //if (ascii != null)
-            //{
-            //    for (int j = 0; j < 5; j++)
-            //    {
-            //        for (int i = 0; i < 4; i++)
-            //        {
-            //            Console.Write(ascii[i, j]);
-            //        }
-
-            //        Console.WriteLine();
-            //    }
-            //}
+           
         }
 
-        public static char[,] GetAscii(char c)
-        {
-            var MonDictionnaire = new Dictionary<char, char[,]>();
-            char[,] A = new char[4, 5];
-            A[0, 0] = ' '; A[0, 1] = '#'; A[0, 2] = '#'; A[0, 3] = '#'; A[0, 4] = '#';
-            A[1, 0] = '#'; A[1, 1] = ' '; A[1, 2] = '#'; A[1, 3] = ' '; A[1, 4] = ' ';
-            A[2, 0] = ' '; A[2, 1] = '#'; A[2, 2] = '#'; A[2, 3] = '#'; A[2, 4] = '#';
-            A[3, 0] = ' '; A[3, 1] = ' '; A[3, 2] = ' '; A[3, 3] = ' '; A[3, 4] = ' ';
-
-
-            MonDictionnaire.Add('A', A);
-            if (MonDictionnaire.ContainsKey(c))
-            {
-                return MonDictionnaire[c];
-            }
-            else
-            {
-                return null;
-            }
-
-
-        }
+        
     }
 }
